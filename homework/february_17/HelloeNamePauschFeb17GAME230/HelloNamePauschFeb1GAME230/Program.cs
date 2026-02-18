d4c4c44798e06e38ad907e517861d46e6a7f703a
Jink2024 <Pausch@chapman.edu>
2026-02-18 11:42:22 -0800

    Calculator

--- homework/february_17/HelloeNamePauschFeb17GAME230/HelloNamePauschFeb1GAME230/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hey, idiot! What can I call you besides idiot?");
-var name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}!");
-Console.WriteLine("That is such a cool name, way better than idiot.");
