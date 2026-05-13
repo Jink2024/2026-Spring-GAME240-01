f4bc8aade04f0088bff1be7f6c29f54169f25ede
Jink2024 <Pausch@chapman.edu>
2026-05-12 17:28:28 -0700

    Add files via upload

---
+++ homework/March12HWPausch.cs
@@ -0,0 +1,47 @@
+﻿
+int[] number = [33, 44, 55, 66, 21];
+// sum the numbers in the array ???
+int biggest;
+int smallest;
+
+if (number[0] > number[1] && number[0] > number[2] && number[0] > number[3] && number[0] > number[4])
+{
+    Console.WriteLine("the biggest number is", number[0]);
+}
+if (number[1] > number[0] && number[1] > number[2] && number[1] > number[3] && number[1] > number[4])
+{
+    Console.WriteLine($"the biggest number is",number[1]);
+}
+if (number[2] > number[0] && number[2] > number[1] && number[2] > number[3] && number[2] > number[4])
+{
+    Console.WriteLine($"the biggest number is",number[2]);
+}
+if (number[3] > number[0] && number[3] > number[1] && number[3] > number[2] && number[3] > number[4])
+{
+    Console.WriteLine($"the biggest number is",number[3]);
+}
+if (number[4] > number[0] && number[4] > number[1] && number[4] > number[2] && number[4] > number[3])
+{
+    Console.WriteLine($"the biggest number is",number[4]);
+}
+
+if (number[0] < number[1] && number[0] < number[2] && number[0] < number[3] && number[0] < number[4])
+{
+    Console.WriteLine($"the smallest number is", number[0]);
+}
+if (number[1] < number[0] && number[1] < number[2] && number[1] < number[3] && number[1] < number[4])
+{
+    Console.WriteLine($"the smallest number is",number[1]);
+}
+if (number[2] < number[0] && number[2] < number[1] && number[2] < number[3] && number[2] < number[4])
+{
+    Console.WriteLine($"the smallest number is",number[2]);
+}
+if (number[3] < number[0] && number[3] < number[1] && number[3] < number[2] && number[3] < number[4])
+{
+    Console.WriteLine($"the smallest number is",number[3]);
+}
+if (number[4] < number[0] && number[4] < number[1] && number[4] < number[2] && number[4] < number[3])
+{
+    Console.WriteLine($"the smallest number is",number[4]);
+}
